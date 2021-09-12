0000ee9c5f4964e79d03c6ac785d6d3bd8570600
Cayman Gill <cayman.g@hotmail.com>
2021-09-12 19:09:43 -0400

    updated user post and comment routes as well as corrected comment.init

--- models/Comment.js
+++ models/Comment.js
@@ -5,7 +5,34 @@ class Comment extends Model {}
 
 Comment.init(
   {
-    // columns will go here
+   id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+   },
+   comment_text: {
+     type: DataTypes.TEXT,
+     allowNull: false,
+     validate: {
+       len: [1, 200]
+     }
+   },
+   user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'user',
+      key: 'id'
+    }
+   },
+   post_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    references: {
+      model: 'post',
+      key: 'id'
+    }
+   }
   },
   {
     sequelize,
--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,50 +1,44 @@
 const router = require('express').Router();
 const { Comment } = require('../../models');
-const { Post, User, Vote, Comment } = require("../../models");
+const { Post, User, Vote } = require("../../models");
 
 router.get('/', (req, res) => {
-    Post.findAll({
-        order: [['created_at', 'DESC']],
-        attributes: [
-          'id',
-          'post_url',
-          'title',
-          'created_at',
-          [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-        ],
-        include: [
-          // include the Comment model here:
-          {
-            model: Comment,
-            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-            include: {
-              model: User,
-              attributes: ['username']
-            }
-          },
-          {
-            model: User,
-            attributes: ['username']
-          }
-        ]
-       })
+  Comment.findAll({
+    attributes: ['id', 'comment_text', 'user_id', 'post_id']
+  }).then(dbData => {
+    res.status(200).json(dbData);
+  }).catch(err => {
+    res.status(500).json({ message: 'failed to receive data', error: err })
+  });
 });
 
 router.post('/', (req, res) => {
-    Comment.create({
-        comment_text: req.body.comment_text,
-        user_id: req.body.user_id,
-        post_id: req.body.post_id
-      })
-        .then(dbCommentData => res.json(dbCommentData))
-        .catch(err => {
-          console.log(err);
-          res.status(400).json(err);
-        });
+  Comment.create({
+    comment_text: req.body.comment_text,
+    user_id: req.body.user_id,
+    post_id: req.body.post_id
+  })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
-
+  Comment.destroy({
+    where: {
+      id: req.params.id
+    }
+  }).then(dbData => {
+    if(dbData[0] === 0){
+      res.status(404).json({message: 'No item with that ID found'});
+      return;
+    }
+    res.status(200).json({message: 'deletion success', data: dbData})
+  }).catch(err => {
+    res.status(500).json({message: 'failed to delete item', error: err})
+  });
 });
 
 module.exports = router;
